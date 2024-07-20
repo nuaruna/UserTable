139d66715ff8ba71faa4f78c64ae0e31d552ddac
dv.popov1 <dv.popov1@tensor.ru>
2024-07-20 17:50:56 +0300

    Initial commit

---
+++ src/interfaces/User.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: number
+  firstName: string | null
+  lastName: string | null
+  email: string | null
+  lastVisitedAt: number
+}
---
+++ src/stores/users.ts
@@ -0,0 +1,23 @@
+import { ref, type Ref } from 'vue'
+import { defineStore } from 'pinia'
+import type { User } from '@/interfaces/User'
+import * as data from '@/userList.json'
+
+export const useUsersStore = defineStore('users', () => {
+  const users: Ref<Map<Number, User>> = ref(new Map())
+
+  function fetchUserList() {
+    // использовал Map из-за малого кол-ва пользователей, можно и обычный массив но тогда придется усложнять saveUser/deleteUser
+    users.value = new Map(data.map((item) => [item.id, item]))
+  }
+
+  function saveUser(value: User) {
+    return users.value.set(value.id, value)
+  }
+
+  function deleteUser(id: Number) {
+    return users.value.delete(id)
+  }
+
+  return { users, fetchUserList, saveUser, deleteUser }
+})
